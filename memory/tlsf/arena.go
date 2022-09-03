20e9b0902b414487b62b0d940b7aec30223555db
cmolocznik <291061+cmolocznik@users.noreply.github.com>
2022-09-03 09:03:29 +0800

    initial checkin

---
+++ memory/tlsf/arena.go
@@ -0,0 +1,11 @@
+//go:build !tinygo
+
+package tlsf
+
+// Arena allocates memory from the underlying platform. It is used to add
+// new memory to an Allocator.
+type Arena interface {
+	Alloc(size uintptr) (uintptr, uintptr)
+
+	Free()
+}
